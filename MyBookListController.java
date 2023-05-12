5112bd4d022a245c22e00ce26390be7f5080017b
ranjitmaneW <131524886+ranjitmaneW@users.noreply.github.com>
2023-05-12 14:24:20 +0530

    Add files via upload

---
+++ MyBookListController.java
@@ -0,0 +1,26 @@
+package com.example.demo.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.Service.MyBookListService;
+
+@Controller
+public class MyBookListController {
+	
+@Autowired	
+private MyBookListService service;
+	
+@RequestMapping	("/deleteMyList/{id}")
+public String deleteMyList(@PathVariable("id")int id)
+{
+	
+service.deleteById(id);
+return"redirect:/my_books";
+
+}
+
+
+}
